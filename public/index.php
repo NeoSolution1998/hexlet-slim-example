7e398193fb3b73fd5f7694b89c3d681f76681f49
neosolution <neosolution1998@gmail.com>
2021-12-23 13:07:07 +0300

    d

--- public/index.php
+++ public/index.php
@@ -4,14 +4,50 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+use function Symfony\Component\String\s;
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
 });
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("this : {$id}");
+});
+
+$app->get('/courses/{courseId}/lessons/{id}', function ($request, $response, array $args) {
+    $courseId = $args['courseId'];
+    $id = $args['id'];
+    return $response->write("Course id: {$courseId}")
+      ->write("<br/>  Lesson id: {$id}");
+});
+
+$app->get('/users', function ($request, $response, $args) use ($users){
+    $term = $request->getQueryParam('term');
+    $result = collect($users)->filter(
+        fn($user) => str_contains($user, $term) ? $user : false
+    );
+    $params = ['users' => $result, 'term' => $term];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
 $app->run();
+
